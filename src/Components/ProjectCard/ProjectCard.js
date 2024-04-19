a2ed107ea3c840709a380c2af56025df55380acc
htnamus <tangiralasumanth@gmail.com>
2024-04-19 18:09:32 -0400

    Added ML SBMP Survey Image

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -3,24 +3,43 @@ import React from 'react';
 import styles from './ProjectCard.module.scss';
 import {useHistory} from "react-router-dom";
 import htmlParse from "html-react-parser";
+import {stripHTML} from "../../helpers";
 
 function ProjectCard({projectDetails}) {
+  const {
+    should_redirect,
+    urlSuffix,
+    imageURL,
+    url,
+  } = projectDetails;
   
   const history = useHistory();
+  
   const handleClick = () => {
-    if (projectDetails.should_redirect){
-      window.open(projectDetails.url, "_blank")
+    if (should_redirect){
+      window.open(url, "_blank")
     }
     else{
-      history.push(projectDetails.urlSuffix)
+      history.push(urlSuffix)
     }
     
   }
+  
+  const renderImage = () => (
+    <img
+      src={imageURL}
+      alt=""
+      className={styles.image}
+    />
+  );
+  
+  
   return (
     <div className={styles.container} onClick={handleClick}>
       <div className={styles.title}>
         {htmlParse(projectDetails.name)}
       </div>
+      {imageURL && renderImage()}
     </div>
   );
 }
