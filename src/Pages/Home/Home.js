5f4e491e965bebe32e9ae44ba18b8b790cdc8321
htnamus <tangiralasumanth@gmail.com>
2024-09-20 15:20:47 -0400

    Updated the design of the home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import React, {useMemo} from 'react';
 import _map from 'lodash/map';
 import _filter from 'lodash/filter';
 import _sortBy from 'lodash/sortBy';
+import _size from 'lodash/size';
 
 import config from 'config';
 
@@ -15,17 +16,36 @@ function Home(props) {
       return _sortBy(projectsToDisplay, ({sortKey}) => -(sortKey || 100));
   }, []);
   
+  const preprints = useMemo(() => {
+    return _filter(projects, ({preprint}) => !!preprint);
+  }, [projects]);
+  
+  const publications = useMemo(() => {
+    return _filter(projects, ({preprint}) => !preprint);
+  }, [projects]);
+  
   return (
-    <div>
+    <div className={styles.container}>
+      {!!_size(preprints) && (
+        <>
+          <div className={styles.projectsHeading}>Pre-Prints</div>
+          <div className={styles.projectList}>
+            {
+              _map(preprints, (projectDetails, projectId) => (
+                <ProjectCard projectDetails={projectDetails} key={projectId}/>
+              ))
+            }
+          </div>
+        </>
+      )}
       <div className={styles.projectsHeading}>Publications</div>
       <div className={styles.projectList}>
         {
-          _map(projects, (projectDetails, projectId) => (
+          _map(publications, (projectDetails, projectId) => (
             <ProjectCard projectDetails={projectDetails} key={projectId}/>
-          ))
-        }
+              ))
+            }
       </div>
-      
     </div>
   );
 }
